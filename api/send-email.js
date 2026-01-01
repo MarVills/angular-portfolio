03d4596e00755a6b9a4072242053e49b91d93099
Marben Villaflor <villaflormarbenc@gmail.com>
2026-01-01 16:05:58 +0800

    fix: options preflight

--- api/send-email.js
+++ api/send-email.js
@@ -5,7 +5,18 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Methods", "POST,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
+  // if (req.method === "OPTIONS") {
+  //   return res.status(200).end();
+  // }
+
   if (req.method === "OPTIONS") {
+    const allowedOrigins = ["https://marvills.github.io", "http://localhost:4200"];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+      res.setHeader("Access-Control-Allow-Origin", origin);
+    }
+    res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     return res.status(200).end();
   }
 
