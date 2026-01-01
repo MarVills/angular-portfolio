6825852d59c890572acce07ef895ea2f9b85912b
Marben Villaflor <villaflormarbenc@gmail.com>
2026-01-01 14:30:44 +0800

    testing: fixing send email request method related issue

--- api/send-email.js
+++ api/send-email.js
@@ -6,6 +6,8 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
+  console.log("req.method", req.method);
+
   if (req.method === "OPTIONS") {
     return res.status(200).end(); // respond to preflight
   }
