1def88b16092e7174d39a49ab30ba582c69c9c37
Marben Villaflor <villaflormarbenc@gmail.com>
2026-01-04 15:17:32 +0800

    fix: updated environment production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://angular-portfolio.vercel.app/api',
-  // apiUrl: '/api',
+  localApiUrl: 'http://localhost:3000', // direct
+  type: 'production', // local | development | staging | production
 };
